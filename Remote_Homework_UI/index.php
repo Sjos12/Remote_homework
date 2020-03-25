1685f8220fb85f4a2cc9a0479bdcfa735e3a9238
Sjos <denzelstellingwerf@outlook.com>
2020-03-25 20:14:10 +0100

    The UI of our website

---
+++ Remote_Homework_UI/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Remote Homework - HOME</title>
+  <!-- Insert your adress bar icon here -->
+  <LINK REL="SHORTCUT ICON"
+       HREF="top-icon.ico">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+  </style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <!-- Grey with black text -->
+    <nav class="navbar sticky-top navbar-expand-sm bg-white navbar-light ">
+        <a class="navbar-brand" href="#">
+            <img src="Better_logo.png" alt="Logo" style="width:105px;">
+        </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#data"><span class="navbar-toggler-icon" style="width:20px; height: 25px;"></span></button>
+
+        <div class="collapse navbar-collapse" id="data">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                <a class="nav-link" href="#content1">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#content2">Info</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#content3">Signup</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#content4">Login</a>
+                </li>
+            </ul>
+        </div> 
+    </nav>
+
+  
+<div class="container" id="#content1">
+  <h3>Homework just got a lot easier</h3>
+  <p>Remote homework is an online tool that makes collaborating remotely on a centurie us PageMaker including verLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumsions of Lorem Ipsumering lan gvmdfo oid eet kaas graag </p>
+  <a href="#" class="btn btn-primary">Get Started</a> 
+</div>
+
+<hr>
+
+<div class="container" id="#content2">
+  <h3>Homework just got a lot easier</h3>
+  <p>Remote homework is an online tool that makes collaborating remotely on a project easy. </p>
+  <a href="#" class="btn btn-primary">Get Started</a> 
+</div>
+</body>
+</html>
+
+
