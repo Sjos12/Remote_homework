e2a078df17f4543ad3e33bd5416f2a0df5774b69
qrazi <qrazi.sivlingworkz@gmail.com>
2020-04-09 09:25:08 +0200

    Show logged in status correctly

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
             <div class="container" id="content1">
       <div class="row startpage">
         <!-- the div that holds the text from content1-->
-        <div class='col-lg-7 col-sm-12' id="content1"> 
+        <div class='col-lg-7 col-sm-12' id="content1">
           <h2>Homework just got a lot easier</h2>
           <p>Remote homework is an online tool that makes collaborating remotely on projects or homework a piece of cake. </p>
           <div class="wrapper">
@@ -32,7 +32,7 @@
       <h4>Easy collaboration</h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
     </div>
-    
+
     <div class="col-lg-4">
       <h4>Workspace</h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
@@ -42,7 +42,7 @@
       <h4>Low cost subscriptions</h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
     </div>
-    
+
     <!--<img src="phone-template.png" alt="phone" class="col-xs-8" id="iphoneimage">-->
   </div>
 </div>
@@ -56,7 +56,7 @@
       <h4>Step 1 </h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
     </div>
-    
+
     <div class="col-lg-4">
       <h4>Step 2 </h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
@@ -66,20 +66,20 @@
       <h4>Step 3</h4>
       <p> The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable</p>
     </div>
-    
+
     <!--<img src="phone-template.png" alt="phone" class="col-xs-8" id="iphoneimage">-->
   </div>
 </div>
 <hr>
-
-                      <!-- checks if user is logged in -->
-                      @if (session('status'))
-                          <div class="alert alert-success" role="alert">
-                              {{ session('status') }}
-                          </div>
-                      @endif
-
-                      You are logged in!
+                @auth
+                    {{-- The user is authenticated... --}}
+                    <div class="alert alert-success" role="alert">
+                    You are logged in!
+                    </div>
+                @endauth
+                @guest
+                    {{-- The user is not authenticated... --}}
+                @endguest
           </div>
       </div>
   </div>
