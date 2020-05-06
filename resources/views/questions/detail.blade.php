c1437ed1838eca8818e86dd19e2a36612236b242
Sjos <denzelstellingwerf@outlook.com>
2020-05-06 18:44:16 +0200

    Visual changes to detail view

--- resources/views/questions/detail.blade.php
+++ resources/views/questions/detail.blade.php
@@ -4,24 +4,24 @@
     <div class="container">
         <div class="row">
             <h3 class="col-6">{{ $question->title }}</h3>
+            <p class="col-10">{{$question->content}}</p>
 
             @if($firstImage)
-            <div class="col-10">
+            <div class="p-5 mx-auto col-10">
                 {{ $firstImage->img()->attributes(['class' => 'img-fluid', 'alt' => $question->title]) }}
             </div>
             @endif
-
-            <p class="col-10">{{$question->content}}</p>
-
-            <p class="col-12 question-data">
-                Submitted by: {{ $question->author->name }}
-            </p>
-            <p class="col-6 question-data">
-                Posted at: {{ $question->created_at->toDateTimeString() }}
-            </p>
-            <p class="mr-auto col-6 question-data text-right">
-                Last updated at: {{ $question->updated_at->toDateTimeString() }}
-            </p>
+    
+                <p class="col-12 question-data">
+                    Submitted by: {{ $question->author->name }}
+                </p>
+                 
+            <p class="col-6 question-data text-left">
+                    Posted at: {{ $question->created_at->toDateTimeString() }}
+                </p>
+                <p class="col-6 question-data text-right">
+                    Last updated at: {{ $question->updated_at->toDateTimeString() }}
+                </p>
         </div>
     </div>
 @endsection
