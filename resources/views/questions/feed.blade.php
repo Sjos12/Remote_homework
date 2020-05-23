6b190dfb3ffcb6fb21262f88297aecc3231a4341
qrazi <qrazi.sivlingworkz@gmail.com>
2020-05-23 11:09:19 +0200

    Improve dark mode colors

--- resources/views/questions/feed.blade.php
+++ resources/views/questions/feed.blade.php
@@ -3,34 +3,38 @@
 @section('content')
     <div class="container">
         @foreach($questions as $view_model)
-            <div class="row pt-4 pb-4">
+            <div class="row ">
                 @if($firstImage = $view_model->firstImage())
-                    <div class=" col-lg-4 col-md-4 col-xs-12 col-sm-12">
-                        {{ $firstImage->img()->attributes(['class' => 'img-fluid  pt-4 pb-4', 'alt' => $view_model->question()->title]) }}
+                    <div class=" col-lg-4 col-md-4 col-xs-12 col-sm-12 d-flex justify-content-center">
+                        {{ $firstImage->img()->attributes(['class' => 'img-fluid d-block mx-auto mt-auto mb-auto', 'alt' => $view_model->question()->title]) }}
                     </div>
                 @endif
-                <div class="pt-4 col-6"> 
-                    <h3 class="">{{ $view_model->question()->title }}</h3>
-                    <p class="">{{$view_model->question()->content}}</p>
+
+                <div class="ml-auto pt-4 pb-4 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+
+                    <h3 class="col-12">{{ $view_model->question()->title }}</h3>
+                    <p class="col-12">{{$view_model->question()->content}}</p>
+
+                    <div class="ml-auto mt-auto mb-auto">
+                            <a class="btn btn-lg btn-primary float-right"
+                               href="{{ route('questions.detail', ['question' => $view_model->question()->uuid]) }}"
+                            >
+                                View Question
+                            </a>
+                    </div>
+
+                    <h6 class="col-12 text-left align-items-end">
+                        Submitted by: {{ $view_model->question()->author->name }}
+                    </h6>
+                <div class="row"></div>
+                    <p class="col-6 text-left">
+                        Posted at: {{ $view_model->question()->created_at->toDateTimeString() }}
+                    </p>
+                    <p class="text-right align-bottom">
+                        Last updated at: {{ $view_model->question()->updated_at->toDateTimeString() }}
+                    </p>
 `               </div>
-                <div class=" ml-auto mt-auto mb-auto col-2">
-                    <a class="btn btn-lg btn-primary float-right"
-                       href="{{ route('questions.detail', ['question' => $view_model->question()->uuid]) }}"
-                    >
-                        View Question
-                    </a>
-                </div>
-                <h6 class="col-12 pt-3  text-left">
-                    Submitted by: {{ $view_model->question()->author->name }}
-                </h6>
-                
-                <p class="col-6 question-data float-left">
-                    Posted at: {{ $view_model->question()->created_at->toDateTimeString() }}
-                </p>
-                <hr id="feedhr">
-                <p class="col-6 question-data text-right">
-                    Last updated at: {{ $view_model->question()->updated_at->toDateTimeString() }}
-                </p>
+
             </div>
             <hr class="feedhr" id="feedhr">
         @endforeach
