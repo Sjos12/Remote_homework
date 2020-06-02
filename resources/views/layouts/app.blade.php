3e0c9bab544c115cbacda48a9a8168cdc1ad7922
Sjos <denzelstellingwerf@outlook.com>
2020-06-03 00:11:56 +0200

    logo update.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
             <!-- Use container-fluid to arrange navbar items from edge to edge -->
             <div class="container">
                 <a class="navbar-brand" href="{{ route('home') }}#content1">
-                    <img id="logo" src="{{ asset('images/better_logo.png') }}" alt="Logo" style="width:105px;">
+                    <img id="logo" src="{{ asset('images/remote_homework_logo.png') }}" alt="Logo" style="width:105px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
