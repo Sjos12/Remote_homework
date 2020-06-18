78e53155f1cf572aeb5c78a5090cf1d7374c0cc7
Sjos <denzelstellingwerf@outlook.com>
2020-06-18 20:06:46 +0200

    First changes of redesign of landing page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+  <div class="vh-100">
+    <div class="row">
+      <div class="col-md-6">
+          <h1 class="title">Remote Homework</h1> 
+          <p>
+          An easy and simple online platform to
+          remotely collaborate and give feedback on (home)work.
+          </p> 
+      </div>
 
+      <div class="col-md-6 col-sm-12">
+        <img src="/images/homeworkgirl.svg" class="" id="homeworkgirl" alt="Homework girl">
+      </div>
+    </div>
+  </div>
 
+  <div class="row">
+    <h1 class="pt-5 pb-5">Content</h1> 
+  </div>
+
+</div>
 @endsection
