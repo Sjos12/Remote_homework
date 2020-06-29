751cf37a00c80eaac8273534d153300cd5859bdc
Sjos <denzelstellingwerf@outlook.com>
2020-06-29 12:24:18 +0200

    Landing page is now completely optimized for mobile and desktop screens

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -24,22 +24,30 @@
     </div>
   </div>
 
-  <div class="row pb-5">
-    <div class="col-6">
+<hr class="homehr"> 
+
+  <div class="row rowspacingbottom mobilespacingtop">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 desktopimg">
       <img src="/images/collaboration.svg" alt="" id="collaboration" srcset="">
     </div>
     
-    <div class="col-6">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
       <h1 class="title ">Help each other</h1> 
       <p class="paragraph">
       Remote Homework makes helping each other with homework a piece of cake. You can reach out to our community as well as to your teacher or parents and ask them questions on your homework remotely. 
 
       </p> 
     </div>
+
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 mobileimg">
+      <img src="/images/collaboration.svg" alt="" id="collaboration" srcset="">
+    </div>
   </div>
 
-  <div class="row pt-5 pb-5">
-    <div class="col-6">
+<hr class="homehr"> 
+
+  <div class="row rowspacingtop rowspacingbottom">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
       
       <h1 class="title ">Also for teachers.</h1> 
       <p class="paragraph">
@@ -47,12 +55,14 @@
       </p>
     </div>
     
-    <div class="col-6">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
       <img src="/images/teacher.svg" alt="" id="collaboration" srcset="">
     </div>
   </div>
 
-  <div class="row pt-5 pb-5">
+<hr class="homehr"> 
+
+  <div class="row rowspacingtop rowspacingbottom">
     <div class="col-12 pb-4">
       <h1 class="title text-center">How do I ask a question?</h1>
       <!--<p class="paragraph text-center">
@@ -60,17 +70,17 @@
       </p>-->
     </div>
 
-    <div class="col-4 phonecontainer">
+    <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 phonecontainer">
       <img src="/images/phone1.svg" alt="" id="phone1" srcset="" class="d-block mt-auto mx-auto">
       <p class="paragraph text-center">Make a picture of the assignment you're struggling with. 
       </p>
     </div>
-    <div class="col-4 phonecontainer">
+    <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 phonecontainer">
       <img src="/images/phone2.svg" alt="" id="phone2" srcset="" class="d-block my-auto mx-auto">
       <p class="paragraph text-center">Make a picture of the assignment you're struggling with. 
       </p>
     </div>
-    <div class="col-4 ">
+    <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 ">
       <img src="/images/phone3.svg" alt="" id="phone3" srcset="" class="d-block mt-auto mx-auto">
       <p class="paragraph text-center">Make a picture of the assignment you're struggling with. 
       </p>
@@ -78,11 +88,13 @@
 
     <h2 class="col-12 title2 d-block mx-auto pt-4">Easy? Isn't it? </br> For more, check out our tutorials here. 
     </h2>
-    <a href="{{ route('login') }}" class="shadow-sm btn btn-primary btn-lg border border-info d-block mx-auto mt-4" id="homebtn2">View tutorials</a>
+    <a href="{{ route('login') }}" class="shadow-sm btn btn-primary btn-lg border border-info d-block mx-auto mt-4" id="homebtn3">View tutorials</a>
   </div>
 
-  <div class="row pt-5 pb-5">
-    <div class="col-6">
+<hr class="homehr"> 
+
+  <div class="row rowspacingtop rowspacingbottom">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
       <div class="col-12">
         <h1 class="title ">What does it cost? </h1> 
         <p class="paragraph">
@@ -90,12 +102,12 @@
         </p>
       </div>
       <div class="col-12">
-        <a href="{{ route('login') }}" class="shadow-sm btn btn-primary btn-lg border border-info mt-4" id="homebtn3">Donate</a>
+        <a href="{{ route('login') }}" class="shadow-sm btn btn-primary btn-lg border border-info mt-4" id="homebtn4">Donate</a>
       </div>
       
     </div>
 
-    <div class="col-6">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
       <img src="/images/superman.svg" alt="" id="collaboration" srcset="">
     </div>
   </div>
