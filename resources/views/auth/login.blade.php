b6c56cb85616f22c2b256bf59daabcb99539c9e6
Sjos <denzelstellingwerf@outlook.com>
2020-07-28 15:58:34 +0200

    New login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
 <div class="container vh-100">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-light">{{ __('Login') }}</div>
-
-                <div class="card-body">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-7 col-sm-12 col-xs-12">
+            <div class="card card-body">
+                <div class="card__content">
+                    <h1 class="text-center">{{ __('Login') }}</h1>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="form-group row d-flex justify-content-center">
+                            <div class="">
+                                <label for="email" class="col-md-12 col-form-label text-left pl-0">{{ __('E-Mail Address') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,10 +23,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row d-flex justify-content-center">
+                            
 
                             <div class="col-md-6">
+                                <label for="password" class="col-md-12 col-form-label text-left pl-0">{{ __('Password') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -39,7 +38,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row ">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
