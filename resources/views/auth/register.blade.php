2291af2f03497181526446db6f427d8ab51938a2
Sjos <denzelstellingwerf@outlook.com>
2020-07-29 17:28:02 +0200

    Applied new form design to register form.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container vh-100">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
+        <div class="col-md-7 col-sm-12 col-xs-12 ">
+            <div class="card card--login card--shadow card-body d-flex justify-content-center">
+                <div class="card__content">
+                    <h1 class="title2 pb-4">{{ __('Register') }}</h1>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <div class="form-group row d-flex justify-content-center">
+                            <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
+                                <label for="name" class="col-md-12 col-form-label text-left pl-0">{{ __('Username') }}</label>
+                           
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror input--shadow " name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +24,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <div class="form-group row d-flex justify-content-center">
+                            <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
+                                <label for="email" class="col-md-12 col-form-label text-left pl-0">{{ __('E-Mail Address') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror input--shadow " name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -39,11 +37,12 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row d-flex justify-content-center">
+                            
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
+                                <label for="password" class="col-md-12 col-form-label text-left pl-0">{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input--shadow " name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -53,17 +52,16 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="form-group row d-flex justify-content-center">
+                            <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
+                                <label for="password-confirm" class="col-md-12 col-form-label text-left pl-0">{{ __('Confirm Password') }}</label>
+                                <input id="password-confirm" type="password" class="form-control input--shadow" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group row mb-0 pt-3">
+                            <div class="mx-auto">
+                                <button type="submit" class="btn btn-primary btn-lg pl-5 pr-5">
                                     {{ __('Register') }}
                                 </button>
                             </div>
