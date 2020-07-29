a80de6cac76249762668886756290e3c2643e0cf
Sjos <denzelstellingwerf@outlook.com>
2020-07-29 10:47:43 +0200

    Added box-shadow to input fields and some padding to title and login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,16 +4,16 @@
 <div class="container vh-100 ">
     <div class="row d-flex justify-content-center">
         <div class="col-md-7 col-sm-12 col-xs-12 ">
-            <div class="card card--login card-body d-flex justify-content-center">
+            <div class="card card--login card--shadow card-body d-flex justify-content-center">
                 <div class="card__content">
-                    <h1 class="title2">{{ __('Login') }}</h1>
+                    <h1 class="title2 pb-4">{{ __('Login') }}</h1>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row d-flex justify-content-center">
                             <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
                                 <label for="email" class="col-md-12 col-form-label text-left pl-0">{{ __('E-Mail Address') }}</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror input--shadow " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
 
                             <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12">
                                 <label for="password" class="col-md-12 col-form-label text-left pl-0">{{ __('Password') }}</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input--shadow" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 pt-3">
                             <div class="mx-auto">
                                 <button type="submit" class="btn btn-primary btn-lg pl-5 pr-5">
                                     {{ __('Login') }}
