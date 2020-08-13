6ebf187dc56447b72959c8b310efd4b6a0596c7c
Sjos <denzelstellingwerf@outlook.com>
2020-08-13 23:26:28 +0200

    Further changes to question card.

--- resources/views/feed/list.blade.php
+++ resources/views/feed/list.blade.php
@@ -24,46 +24,47 @@
         
         <div class="row">
             <div class="card col-12">
-                @if($firstImage = $view_model->firstImage())
-                    <div class="img-container col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                        <div class="img-box">
-                            {{ $firstImage->img()->attributes(['class' => 'question-img img-fluid d-block mx-auto mt-auto mb-auto rounded', 'alt' => $view_model->question()->title]) }}
+                <div class="row col-10 offset-1">
+                    <div class="content-container ml-auto p-0 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+
+                        <h3 class="title5">{{ $view_model->question()->title }}</h3>
+                        <p class="mb-auto paragraph">{{$view_model->question()->content}}</p>
+
+                        <div class="ml-auto mt-auto mb-auto">
+                                <a class="btn btn-lg btn-primary float-right"
+                                href="{{ route('feed.detail', ['question' => $view_model->question()->uuid]) }}"
+                                >
+                                    View Question
+                                </a>
                         </div>
+
+                        <div class="col-12 d-flex justify-content-center"></div>
+                        <h6 class="">
+                            Asked by: {{ $view_model->question()->author->name }}
+                        </h6>
+                        <p class="">
+                            Asked  {{ $view_model->createdSince() }}
+                        </p>
+
+                        <p class="">
+                            Last updated {{ $view_model->createdSince() }}
+                        </p>
                     </div>
+                    
+                    @if($firstImage = $view_model->firstImage())
+                        <div class="img-container col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                            <div class="img-box">
+                                {{ $firstImage->img()->attributes(['class' => 'question-img img-fluid d-block mx-auto mt-auto mb-auto rounded', 'alt' => $view_model->question()->title]) }}
+                            </div>
+                        </div>
 
-                    <!-- <div class=" col-lg-4 col-md-4 col-xs-12 col-sm-12 d-flex justify-content-center">
-                       
-                    </div> -->
-                @endif
-
-                <div class="content-container ml-auto pt-4 pb-4 col-lg-8 col-md-8 col-sm-12 col-xs-12">
-
-                    <h3 class="col-12 title3">{{ $view_model->question()->title }}</h3>
-                    <p class="col-12 mb-auto paragraph">{{$view_model->question()->content}}</p>
-
-                    <div class="ml-auto mt-auto mb-auto">
-                            <a class="btn btn-lg btn-primary float-right"
-                               href="{{ route('feed.detail', ['question' => $view_model->question()->uuid]) }}"
-                            >
-                                View Question
-                            </a>
-                    </div>
-
-                    <div class="col-12 d-flex justify-content-center"></div>
-                    <h6 class="col-12 ">
-                        Asked by: {{ $view_model->question()->author->name }}
-                    </h6>
-                    <p class="col-6 ">
-                        Asked  {{ $view_model->createdSince() }}
-                    </p>
-
-                    <p class="col-6">
-                        Last updated {{ $view_model->createdSince() }}
-                    </p>
-`               </div>
-
-             </div>
-            </div>              
+                        <!-- <div class=" col-lg-4 col-md-4 col-xs-12 col-sm-12 d-flex justify-content-center">
+                        
+                        </div> -->
+                    @endif
+                </div>
+            </div>
+        </div>              
         @endforeach
 
     </div>
