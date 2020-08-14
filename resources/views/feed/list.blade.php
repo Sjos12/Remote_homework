a669c6399eb95ce317dbf786f289ca3d38b1f62c
Sjos <denzelstellingwerf@outlook.com>
2020-08-14 09:13:29 +0200

    Styled card info and quesiton cards itself. Also added Public and Private tabs.

--- resources/views/feed/list.blade.php
+++ resources/views/feed/list.blade.php
@@ -25,39 +25,40 @@
         <div class="row">     
             <div class="card card--list col-12"> 
                 <div class="row">
-                    <div class="col-10 offset-1">
-                        <h1>Public</h1>
-                        <h1>Private</h1>
+                    <div class="row col-10 offset-1">
+                        <a href="" class="title">Public</a>
+                        <a href="" class="title5">Private</a>
                     </div>
                 </div>  
             @foreach($questions as $view_model)
-                <div class="row">
-                    <div class="card card--question col-12">
-                        <div class="row col-10 offset-1">
-                            <div class="content-container ml-auto p-0 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                <div class="row pb-5 pl-4 pr-4">
+                    <div class="card card--question card--question--padding col-12">
+                        <div class="row p-0 col-10 offset-1">
+                            <div class="content-container ml-auto p-0 col-lg-5 col-md-5 col-sm-12 col-xs-12">
 
-                                <h3 class="title5">{{ $view_model->question()->title }}</h3>
-                                <p class="mb-auto paragraph">{{$view_model->question()->content}}</p>
-
-                                <div class="ml-auto mt-auto mb-auto">
-                                        <a class="btn btn-lg btn-primary float-right"
-                                        href="{{ route('feed.detail', ['question' => $view_model->question()->uuid]) }}"
-                                        >
-                                            View Question
-                                        </a>
+                                <div class="">
+                                    <h3 class="title5">{{ $view_model->question()->title }}</h3>
+                                    
+                                    <p class="mb-auto paragraph">{{$view_model->question()->content}}</p>
                                 </div>
+                                
+                                <div class=" card--question- mt-auto">  
+                                    <div class="div1">
+                                        <h6 class="card--question--info">
+                                            Asked by {{ $view_model->question()->author->name }} <img src="/images/circle.svg" alt="circle">
+                                            {{ $view_model->createdSince() }}
+                                        </h6>
+                                    </div>
+                                </div>
+                           
+                            </div>
 
-                                <div class="col-12 d-flex justify-content-center"></div>
-                                <h6 class="">
-                                    Asked by: {{ $view_model->question()->author->name }}
-                                </h6>
-                                <p class="">
-                                    Asked  {{ $view_model->createdSince() }}
-                                </p>
-
-                                <p class="">
-                                    Last updated {{ $view_model->createdSince() }}
-                                </p>
+                            <div class="col-3 d-flex justify-content-center mt-auto mb-auto">
+                                <a class="btn btn-lg btn-primary float-right pl-5 pr-5"
+                                href="{{ route('feed.detail', ['question' => $view_model->question()->uuid]) }}"
+                                >
+                                    View
+                                </a>
                             </div>
                             
                             @if($firstImage = $view_model->firstImage())
@@ -71,12 +72,11 @@
                                 
                                 </div> -->
                             @endif
+                            </div>
                         </div>
                     </div>
-                </div>    
+                @endforeach    
             </div>
         </div>          
-        @endforeach
-
     </div>
 @endsection
