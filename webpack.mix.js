1549b033b472c4ae4778da05eea45a06299cfcb4
qrazi <qrazi.sivlingworkz@gmail.com>
2020-08-29 10:54:35 +0200

    Fix syntax error, add cache-busting, ignore mix-manifest

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-
-mix.js('resources/js/index.js', 'public/js')
-   //.js('resources/js/tools.js', 'public/js')
+    .js('resources/js/index.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .version()
