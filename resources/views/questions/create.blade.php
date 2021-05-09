b6888575a910138fa01477b5e761855e8c1cec39
sjos12 <denzelstellingwerf@outlook.com>
2021-05-09 12:59:21 +0200

    Styled the buttons.

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -93,13 +93,13 @@
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group">
-                            <button onclick="addSlide()" type="button">
+                        <div class="form-group col-12 col-md-8 offset-md-2">
+                            <button onclick="addSlide()" type="button" class="btn btn-primary">
                                 Add slide.
                             </button>
-                            <button onclick="removeSlide()" type="button">Remove slide</button>
+                            <button onclick="removeSlide()" type="button" class="btn btn-secondary">Remove slide</button>
                         </div>
-                        <div class="form-group form-group col-12 col-md-8 offset-md-2" id="form-group__imagedrop">
+                        <div class="form-group col-12 col-md-8 offset-md-2" id="form-group__imagedrop">
                             <div class="carousel-wrapper">
                                 <div class="carousel" id="carousel">
                                     
@@ -128,10 +128,11 @@
                                     </div>
                                     
                                     <button type="button" onclick="movePrev()" class="carousel__button--previous">
-                                        Previous
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z"/><path d="M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z" fill="rgba(255,255,255,1)"/></svg>
+                                        
                                     </button>
                                     <button type="button" onclick="moveNext()" class="carousel__button--next">
-                                        Next
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z"/><path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z" fill="rgba(255,255,255,1)"/></svg>
                                     </button>
                                 </div>
                                
